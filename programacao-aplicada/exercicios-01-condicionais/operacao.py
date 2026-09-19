8b9d6f2fa218764adf611775ba6cdf4733620227
Lucas <lucassouza170308@gmail.com>
2026-09-19 20:55:55 -0300

    Adicionando Exercício 1 - Condicionais

---
+++ programacao-aplicada/exercicios-01-condicionais/operacao.py
@@ -0,0 +1,21 @@
+
+"""
+Exercício 1: Estruturas Condicionais e Operações Matemáticas
+
+Receba um número inteiro positivo.
+- Se for par, calcule seu quadrado.
+- Se for ímpar, calcule seu cubo.
+"""
+
+num = int(input("Digite um número inteiro positivo: "))
+
+if num <= 0:
+    print("Erro: digite um número positivo.")
+
+elif num % 2 == 0:
+    quadrado = num ** 2
+    print(f"O quadrado de {num} é: {quadrado}")
+
+else:
+    cubo = num ** 3
+    print(f"O cubo de {num} é: {cubo}")
