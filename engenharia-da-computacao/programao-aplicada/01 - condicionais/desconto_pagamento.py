10d25d3c842a12647e9b252dd00be755d0e5ee69
Lucas <lucassouza170308@gmail.com>
2026-09-19 21:29:31 -0300

    Adicionando Exercicio 09 - Condicionais

---
+++ engenharia-da-computacao/programao-aplicada/01 - condicionais/desconto_pagamento.py
@@ -0,0 +1,19 @@
+valorFinal = float(input("Digite o valor total da compra: "))
+formaPagamento = int(input("Informe a condição de pagamento:"))
+
+if formaPagamento == 1:
+  percentual = 0.15
+
+elif formaPagamento == 2:
+  percentual = 0.10
+
+elif formaPagamento == 3:
+  percentual = 0.05
+
+else:
+  percentual = 0
+
+if percentual > 0:
+  print(f"O valor final a ser pago é: {valorFinal * (1 - percentual):.2f}")
+else:
+  print("Condição de pagamento inválida!")
