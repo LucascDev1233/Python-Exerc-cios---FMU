696149beb2f69ff10071670acd7da9d37dcaec7e
Lucas <lucassouza170308@gmail.com>
2026-09-26 02:15:50 -0300

    nothing

--- engenharia-da-computacao/programao-aplicada/01 - condicionais/desconto_pagamento.py
+++ engenharia-da-computacao/programao-aplicada/01 - condicionais/desconto_pagamento.py
@@ -1,15 +1,6 @@
 valorFinal = float(input("Digite o valor total da compra: "))
 condicaoPagamento = int(input("Informe a condição de pagamento:"))
 
-<<<<<<< HEAD
-if formaPagamento == 1:
-  percentual = 0.15
-
-elif formaPagamento == 2:
-  percentual = 0.10
-
-elif formaPagamento == 3:
-=======
 if condicaoPagamento == 1:
   percentual = 0.15
 
@@ -17,7 +8,6 @@ elif condicaoPagamento == 2:
   percentual = 0.10
 
 elif condicaoPagamento == 3:
->>>>>>> test
   percentual = 0.05
 
 else:
